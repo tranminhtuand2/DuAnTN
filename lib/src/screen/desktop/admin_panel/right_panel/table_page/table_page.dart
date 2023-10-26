9f5dfc06c2b3c58a7c39ab99365ae7ec44c373f2
hathechi <thechi1832000@gmail.com>
2023-10-27 03:34:41 +0700

    new code

--- lib/src/screen/desktop/admin_panel/right_panel/table_page/table_page.dart
+++ lib/src/screen/desktop/admin_panel/right_panel/table_page/table_page.dart
@@ -1,16 +1,11 @@
 import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:managerfoodandcoffee/src/common_widget/cache_image.dart';
-import 'package:managerfoodandcoffee/src/controller_getx/brightness_controller.dart';
+import 'package:managerfoodandcoffee/src/controller_getx/table_controller.dart';
 import 'package:managerfoodandcoffee/src/firebase_helper/firebasestore_helper.dart';
-import 'package:managerfoodandcoffee/src/model/table_model.dart';
 import 'package:managerfoodandcoffee/src/screen/desktop/admin_panel/right_panel/table_page/widgets/first_widget.dart';
 import 'package:managerfoodandcoffee/src/screen/desktop/admin_panel/right_panel/table_page/widgets/second_widget.dart';
-import 'package:managerfoodandcoffee/src/screen/desktop/pageadmin/DieuChinh/giohang/giohang_admin.dart';
 import 'package:managerfoodandcoffee/src/utils/colortheme.dart';
-import 'package:managerfoodandcoffee/src/utils/size.dart';
-import 'package:managerfoodandcoffee/src/utils/texttheme.dart';
 
 class TablePage extends StatefulWidget {
   const TablePage({super.key});
@@ -20,9 +15,9 @@ class TablePage extends StatefulWidget {
 }
 
 class _TablePageState extends State<TablePage> {
-  // final valueNotifier = ValueNotifier(false);
   bool isExpanded = false;
   bool thanhtoan = false;
+  final tableController = Get.put(TableController());
 
   @override
   Widget build(BuildContext context) {
@@ -81,44 +76,33 @@ class _TablePageState extends State<TablePage> {
         builder: (
           BuildContext context,
           BoxConstraints constraints,
-        ) =>
-            Row(
-          children: [
-            AnimatedContainer(
-              width: !isExpanded
-                  ? constraints.maxWidth
-                  : constraints.maxWidth / 1.5,
-              duration: const Duration(milliseconds: 1000),
-              curve: Curves.fastOutSlowIn,
+        ) {
+          return Obx(
+            () => Row(
+              children: [
+                AnimatedContainer(
+                  width: !tableController.isExpanded.value
+                      ? constraints.maxWidth
+                      : constraints.maxWidth / 1.5,
+                  duration: const Duration(milliseconds: 1000),
+                  curve: Curves.fastOutSlowIn,
 
-              child: WillPopScope(
-                  onWillPop: () async => true,
-                  child: FirstWidget(
-                    isExpandedValue: isExpanded,
-                    isExpanded: (value) {
-                      setState(() {
-                        isExpanded = value;
-                      });
-                    },
-                  )), //,
+                  child: WillPopScope(
+                      onWillPop: () async => true,
+                      child: const FirstWidget()), //,
+                ),
+                AnimatedContainer(
+                  width: !tableController.isExpanded.value
+                      ? 0
+                      : constraints.maxWidth - constraints.maxWidth / 1.5,
+                  duration: const Duration(milliseconds: 1000),
+                  curve: Curves.fastOutSlowIn,
+                  child: const SecondWidget(),
+                ),
+              ],
             ),
-            AnimatedContainer(
-              width: !isExpanded
-                  ? 0
-                  : constraints.maxWidth - constraints.maxWidth / 1.5,
-              duration: const Duration(milliseconds: 1000),
-              curve: Curves.fastOutSlowIn,
-              child: SecondWidget(
-                isExpanded: (value) {
-                  setState(() {
-                    isExpanded = value;
-                  });
-                },
-                isExpandedValue: isExpanded,
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
