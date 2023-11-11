bed8bb394dd17d204b6b4b48573fa1cefee93d6f
hathechi <thechi1832000@gmail.com>
2023-11-11 19:16:30 +0700

    update flutter maps

---
+++ flutter_google_places_web/lib/src/search_results_tile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_google_places_web/src/flutter_google_places_web.dart';
+
+class SearchResultsTile extends StatelessWidget {
+  final Address addressData;
+  final Function(Address) callback;
+  final Map<String, String> address;
+  SearchResultsTile({
+    required this.addressData, 
+    required this.callback, 
+    required this.address});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: Text(
+          '${addressData.name}',
+          style: TextStyle(fontSize: 16),
+        ),
+        dense: true,
+        onTap: () {
+          callback(addressData);
+        });
+  }
+}
