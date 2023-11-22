22972e1fdf6083b6d159f50f754a529b9510a494
phong phong <nongvanphong21012002@gmail.com>
2023-11-22 10:03:04 +0700

    thử lại lần 2

--- lib/src/screen/mobile/check_location_page.dart
+++ lib/src/screen/mobile/check_location_page.dart
@@ -150,7 +150,7 @@ class _LocationCheckPageState extends State<LocationCheckPage> {
 // mới sửas
 // mới sửas//
 // mới sửas//
-// mới sửas// mớ
+// mới sửas// mớđ
 // i sửas// mới sửas// mới sửas// mới sửas// mới sửas// mới sửas
 
 
